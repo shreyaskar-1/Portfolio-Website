639930acf00b7ed423ee4119265a2cf17d036a18
shreyaskar-1 <shreyaskar21@gmail.com>
2024-08-06 16:56:52 +0530

    changes

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useRef, Suspense } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial, Preload } from '@react-three/drei';
+// @ts-ignore
 import * as random from 'maath/random/dist/maath-random.esm';
 
 const StarBackground = (props: any) => {
