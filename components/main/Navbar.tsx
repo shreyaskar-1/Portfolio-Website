6353f85415a7032270e97af0b8cc5ee9c895f255
shreyaskar-1 <shreyaskar21@gmail.com>
2024-08-06 16:24:18 +0530

    New Commit

---
+++ components/main/Navbar.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Image from "next/image"
+import { Socials } from '@/constants'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2a0e61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10'>
+      <div className='w-full h-full flex flex-row items-center justify-between m-auto px-[10px]'>
+        <a href='#about-me' className='h-auto w-auto flex flex-row items-center'>
+          <Image
+            src='/NavLogo.png'
+            alt='Logo'
+            width={70}
+            height={70}
+            className="cursor-pointer hover:animate-slowspin"
+          />
+          <span className='font-bold ml-[10px] hidden md:block text-gray-300'>
+            Shreyaskar
+          </span>
+        </a>
+
+        <div className='w-[500px] flex flex-row items-center justify-between md:mr-20'>
+          <div className='flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200'>
+            <a href='#about-me' className='cursor-pointer'>About Me</a>
+            <a href='#skills' className='cursor-pointer'>Skills</a>
+            <a href='#projects' className='cursor-pointer'>Project</a>
+            <a href='#contact-us' className='cursor-pointer'>Contact Us</a>
+          </div>
+        </div>
+
+        <div className='flex flex-row gap-5'>
+          {Socials.map((social) => (
+            <Image
+              src={social.src}
+              alt={social.name}
+              key={social.name}
+              width={24}
+              height={24}
+            />
+          ))}
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ components/sub/SkillText.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React from 'react'
+import {motion} from 'framer-motion'
+import { slideInFromRight, slideInFromLeft, slideInFromTop } from '@/utils/motion'
+import { SparklesIcon } from '@heroicons/react/24/solid'
+
+const SkillText = () => {
+  return (
+    <div className='w-full h-auto flex flex-col items-center justify-center'>
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            Think Better Make Better
+          </h1>
+        </motion.div>
+        <motion.div
+        variants={slideInFromLeft(0.5)}
+        className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]'
+        >
+            Making Apps With Modern Technologies
+        </motion.div>
+        <motion.div
+        variants={slideInFromRight(0.5)}
+        className='cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center'
+
+        >
+            Never Miss A Task , Deadline or Idea
+        </motion.div>
+    </div>
+  )
+}
+
+export default SkillText
