97267589dd63c0be55fa9af6a0305b93d200ef48
shreyaskar-1 <shreyaskar21@gmail.com>
2024-08-08 10:07:14 +0530

    changes

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import ProjectCard from '../sub/ProjectCard'
+import React from 'react';
+import ProjectCard from '../sub/ProjectCard';
 
 const Projects = () => {
   return (
     <div className='flex flex-col items-center justify-center py-20'>
-        <h1 className='text-[40px] font-semibld text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'> My Projects</h1>
+        <h1 className='text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'>
+            My Projects
+        </h1>
 
-        <div className='h-full w-full flex flex-col md:flex-row gapa-10 px-10'>
+        <div className='h-full w-full flex flex-col md:flex-row gap-10 px-10'>
             <ProjectCard
-            src="/NextWebsite.png"
-            title='Modern Next.js Portfolio'
-            decription='A modern portfolio website built with Next.js, Tailwind CSS, and TypeScript'
+                src="/NextWebsite.png"
+                title='Modern Next.js Portfolio'
+                description='A modern portfolio website built with Next.js, Tailwind CSS, and TypeScript'
             />
             <ProjectCard
-            src="/CardImage.png"
-            title='Modern Next.js Portfolio'
-            decription='A modern portfolio website built with Next.js, Tailwind CSS, and TypeScript'
+                src="/CardImage.png"
+                title='Modern Next.js Portfolio'
+                description='A modern portfolio website built with Next.js, Tailwind CSS, and TypeScript'
             />
             <ProjectCard
-            src="/SpaceWebsite.png"
-            title='Modern Next.js Portfolio'
-            decription='A modern portfolio website built with Next.js, Tailwind CSS, and TypeScript'
+                src="/SpaceWebsite.png"
+                title='Modern Next.js Portfolio'
+                description='A modern portfolio website built with Next.js, Tailwind CSS, and TypeScript'
             />
         </div>
-
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
