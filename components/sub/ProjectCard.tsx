a15a388a3042aab4c88a84be77912c7581723787
shreyaskar-1 <shreyaskar21@gmail.com>
2024-08-08 10:00:28 +0530

    changes

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import Image from 'next/image';
 
-interface Props{
+interface Props {
     src: string;
-    title:string;
-    decription:string
+    title: string;
+    description: string;
 }
 
-const ProjectsCard = ({src,title,description} : Props) => {
-  return (
-    <div>
-        <Image
-        src={src}
-        alt={title}
-        width={1000}
-        height={1000}
-        className='w-full object-contain'
-        />
-        <div className='relative p-4'>
-            <h1 className='text-2xl font-semibold text-white'>{title}</h1>
-            <p className='mt-2text-gray-300'>{description}</p>
+const ProjectsCard = ({ src, title, description }: Props) => {
+    return (
+        <div>
+            <Image
+                src={src}
+                alt={title}
+                width={1000}
+                height={1000}
+                className='w-full object-contain'
+            />
+            <div className='relative p-4'>
+                <h1 className='text-2xl font-semibold text-white'>{title}</h1>
+                <p className='mt-2 text-gray-300'>{description}</p>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
-export default ProjectsCard
+export default ProjectsCard;
