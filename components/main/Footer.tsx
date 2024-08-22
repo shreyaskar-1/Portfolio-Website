b76d5237a8774bc6b0019622da4d76c14f481519
shreyaskar-1 <shreyaskar21@gmail.com>
2024-08-22 15:20:59 +0530

    changes

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -11,7 +11,7 @@ import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px]">
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] z-20">
       <div className="w-full flex flex-col items-center justify-center m-auto">
         <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
           <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
@@ -26,7 +26,7 @@ const Footer = () => {
               <span className="text-[15px] ml-[6px]">GitHub</span>
             </a>
             <a
-              href="https://leetcode.com/your-leetcode-profile"
+              href="https://leetcode.com/your-actual-profile"
               target="_blank"
               rel="noopener noreferrer"
               className="flex flex-row items-center my-[15px] cursor-pointer"
@@ -35,7 +35,7 @@ const Footer = () => {
               <span className="text-[15px] ml-[6px]">LeetCode</span>
             </a>
             <a
-              href="https://www.youtube.com/your-youtube-channel"
+              href="https://www.youtube.com/your-actual-channel"
               target="_blank"
               rel="noopener noreferrer"
               className="flex flex-row items-center my-[15px] cursor-pointer"
@@ -84,9 +84,12 @@ const Footer = () => {
                 web solutions and mentoring <br /> aspiring developers.
               </span>
             </p>
-            <p className="flex flex-row items-center my-[15px] ml-[-40px] cursor-pointer">
-              <span className="text-[15px] ml-[6px]">shreyaskar21@gmail.com</span>
-            </p>
+            <a
+              href="mailto:shreyaskar21@gmail.com"
+              className="flex flex-row items-center my-[15px] cursor-pointer"
+            >
+              <span className="text-[15px] ml-[-40px]">shreyaskar21@gmail.com</span>
+            </a>
           </div>
         </div>
 
